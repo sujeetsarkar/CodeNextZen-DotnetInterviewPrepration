5c61b1f0062eb35b84a881f2aa523432027ec030
Sujeet Kumar <iamsujeetsarkar@gmail.com>
2019-12-02 19:57:11 +0530

    ref and out usages
    adding ref and out usages example

--- DotNetInterviewPrepration/CodeNextZen-Out-Ref-Params/Program.cs
+++ DotNetInterviewPrepration/CodeNextZen-Out-Ref-Params/Program.cs
@@ -6,7 +6,25 @@ namespace CodeNextZen_Out_Ref_Params
     {
         static void Main(string[] args)
         {
-            
+            //OutUsage
+            Divide(10, 3, out int res, out int rem);
+            Console.WriteLine("{0} and {1}", res, rem); // Outputs "3 and 1"
+
+            //SwapExample
+            int i = 1, j = 2;
+            Swap(ref i, ref j);
+            Console.WriteLine($"{i} {j}");    // Outputs "2 1"
+        }
+        static void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        static void Divide(int x, int y, out int result, out int remainder)
+        {
+            result = x / y;
+            remainder = x % y;
         }
     }
 }
